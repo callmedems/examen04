c0af095a21e596b731750d3f0b398cca913747b8
callmedems <[demmi.zepeda@gmail.com]>
2024-11-26 17:52:25 -0600

    added displayMenu

---
+++ main.cpp
@@ -0,0 +1,20 @@
+// main donde está el menu
+#include <iostream> 
+
+using namespace std; 
+
+void displayMenu() {
+    cout << "==== MENU ====" << endl; 
+    cout << "1. Crear amistad.";
+    cout << "2. Agregar contenido."; 
+    cout << "3. Agregar interés a un usuario.";
+    cout << "4. Ver recomendaciones.";
+    cout << "5. Agregar usuario."; 
+    cout << "6. Salir.";
+}
+
+int main() {
+    
+
+    return 0; 
+}
