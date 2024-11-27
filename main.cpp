b4a8a8d961427852b892154fe8c1461419bea5dd
callmedems <[demmi.zepeda@gmail.com]>
2024-11-26 18:43:46 -0600

    estrcuturé el menu.cpp, llamando a ReccomendationSystem.h

--- main.cpp
+++ main.cpp
@@ -3,18 +3,8 @@
 
 using namespace std; 
 
-void displayMenu() {
-    cout << "==== MENU ====" << endl; 
-    cout << "1. Crear amistad.";
-    cout << "2. Agregar contenido."; 
-    cout << "3. Agregar interés a un usuario.";
-    cout << "4. Ver recomendaciones.";
-    cout << "5. Agregar usuario."; 
-    cout << "6. Salir.";
-}
-
 int main() {
-    
+
 
     return 0; 
 }
