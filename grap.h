fd2fccfb90065d93b13c5fc7cc511f41f0ecd65d
Ariana Villalobos <156480421+Peripera@users.noreply.github.com>
2024-11-26 18:45:31 -0600

    Graph funciones

--- grap.h
+++ grap.h
@@ -12,9 +12,9 @@ using namespace std;
 class Graph {
 public:
     void addUser(const string& usuario); //usuario 
-    void addfrien(const string& usuario1, const string& usuario2);//agregar amistad
+    void addfriend(const string& usuario1, const string& usuario2);//agregar amistad
     set<string> bfs(const string& usuario) const; //algortimo bfs
-    bool existe (const string& usuario) const;//usuario dentro del grafo
+    bool exist (const string& usuario) const;//usuario dentro del grafo
 
 private:
     unordered_map<string, vector<string>> adjList;// estructura del grafo 
